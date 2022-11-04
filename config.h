dc5b784e98e884ef5000c10f4a207f97e9802591
hoehlrich <hoehlrich@raleighcharterhs.org>
2022-11-04 01:51:02 -0400

    changed size, colors, and added fuzzy

--- config.h
+++ config.h
@@ -2,25 +2,25 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;  
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { 
-    "monospace:size=12", "FiraCode Nerd Font Mono:size=20"
+static const char *fonts[] = {
+	"monospace:size=10"
 };
-static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
-
-static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#504945";
-static const char col_gray3[]       = "#bdae93";
-static const char col_gray4[]       = "#ebdbb2";
-static const char col_cyan[]        = "#cc241d";
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { col_gray3, col_gray1},
-	[SchemeSel] = { col_gray4, col_cyan},
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = {"#fbf1c7", "#3c3836" },
+	[SchemeSel]  = { "#fbf1c7", "#d65d0e" },
+	[SchemeOut] = { "#fbf1c7", "#d65d0e" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 30;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
